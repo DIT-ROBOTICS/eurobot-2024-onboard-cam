8264818d9cef65cd8c8183d45f4bb99ed2f0c40f
pomelo925 <yoseph.huang@gmail.com>
2024-03-09 23:58:35 +0800

    ssss

--- inspection/docker/docker-diff.sh
+++ inspection/docker/docker-diff.sh
@@ -5,4 +5,6 @@ find /home/vision/pomelo925/eurobot-2024-vision-main -type f -name "*.sh" -exec 
 export DISPLAY=localhost:10.0
 docker compose -p inspection up -f compose-diff-build.yml
 
+#docker compose down the container and run the below one
+
 docker compose -p inspection up -f compose-diff-run.yml -d
