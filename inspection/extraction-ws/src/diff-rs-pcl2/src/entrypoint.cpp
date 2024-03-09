d657b8463f94aa24216da0ccf0a85609f7e45c9b
pomelo925 <yoseph.huang@gmail.com>
2024-03-10 01:43:05 +0800

    0310 inspection system confirmed

---
+++ inspection/extraction-ws/src/diff-rs-pcl2/src/entrypoint.cpp
@@ -0,0 +1,37 @@
+#include "ros/ros.h"
+#include "geometry_msgs/PointStamped.h"
+#include "std_srvs/SetBool.h" 
+
+ros::ServiceClient client;
+
+
+// Callback for the topic subscriber
+void readySignalCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
+  std_srvs::SetBool srv;
+  srv.request.data = true; 
+
+  // Check frame_id to see if we should proceed with the service ca
+  if (msg->header.frame_id == "inspection") {
+    if (client.call(srv)) system("/home/startup/diff/diff-inspection.sh");
+    else ROS_ERROR("Failed to call service.");
+  }
+  
+  else {
+    srv.request.data = false; 
+    if (!client.call(srv)) ROS_ERROR("Failed to call service.");
+  }
+
+  ros::shutdown();
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ready_signal_processor");
+    ros::NodeHandle nh;
+
+    client = nh.serviceClient<std_srvs::SetBool>("/robot/startup/ready_signal");
+    ros::Subscriber sub = nh.subscribe("/robot/startup/ready_signal", 1000, readySignalCallback);
+
+    ros::spin();
+
+    return 0;
+}
