fd939db207dbfe55231ddad6a45f0bee229e44be
pomelo925 <yoseph.huang@gmail.com>
2024-03-11 23:47:11 +0800

    realsense-ws catkin make

---
+++ inspection/realsense-ws/build/image_pipeline/image_rotate/CMakeFiles/image_rotate_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/realsense-ws/devel/include/image_rotate/ImageRotateConfig.h"
+  "/home/realsense-ws/devel/lib/python3/dist-packages/image_rotate/cfg/ImageRotateConfig.py"
+  "/home/realsense-ws/devel/share/image_rotate/docs/ImageRotateConfig-usage.dox"
+  "/home/realsense-ws/devel/share/image_rotate/docs/ImageRotateConfig.dox"
+  "/home/realsense-ws/devel/share/image_rotate/docs/ImageRotateConfig.wikidoc"
+  "CMakeFiles/image_rotate_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/image_rotate_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
