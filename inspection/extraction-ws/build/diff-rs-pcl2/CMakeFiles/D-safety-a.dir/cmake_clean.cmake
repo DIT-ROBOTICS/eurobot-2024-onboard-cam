53837b49b1891c4057be1e24c30c8cd076955562
HungPinChiu <chiuredpin31060513@gmail.com>
2024-04-30 04:12:59 +0800

    solve min distanse

---
+++ inspection/extraction-ws/build/diff-rs-pcl2/CMakeFiles/D-safety-a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/extraction-ws/devel/lib/diff-rs-pcl2/D-safety-a"
+  "/home/extraction-ws/devel/lib/diff-rs-pcl2/D-safety-a.pdb"
+  "CMakeFiles/D-safety-a.dir/src/safety-a.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D-safety-a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ inspection/extraction-ws/build/diff-rs-pcl2/CMakeFiles/D-safety-b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/extraction-ws/devel/lib/diff-rs-pcl2/D-safety-b"
+  "/home/extraction-ws/devel/lib/diff-rs-pcl2/D-safety-b.pdb"
+  "CMakeFiles/D-safety-b.dir/src/safety-b.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D-safety-b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
